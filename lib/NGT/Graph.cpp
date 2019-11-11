9e981cd29997149e32db7f0c86f7caf1d032f1f3
root <root@bi-iteb-srvr05.engr.uconn.edu>
2019-11-11 07:10:08 -0500

    debug

--- lib/NGT/Graph.cpp
+++ lib/NGT/Graph.cpp
@@ -234,6 +234,8 @@ NeighborhoodGraph::setupSeeds(NGT::SearchContainer &sc, ObjectDistances &seeds, 
 
     setupSeeds(sc, seeds, results, unchecked, distanceChecked);
     cerr << "seeds' size: " << seeds.size() << endl;
+    for(int i=0; i<seeds.size(); i++)
+        cerr << "seed: " << seeds[i] << endl;
 
     Distance explorationRadius = sc.explorationCoefficient * sc.radius;
     const size_t dimension = objectSpace->getPaddedDimension();
